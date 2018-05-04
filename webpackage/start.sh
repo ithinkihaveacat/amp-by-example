baff5cbb34c82261c97547a851a6ffd06f383db1
Michael Stillwell <mjs@beebo.org>
2018-05-04 17:02:01 +0100

    Mostly works--?

--- webpackage/start.sh
+++ webpackage/start.sh
@@ -2,10 +2,13 @@
 
 nohup caddy &
 
-if [ ! -f /letsencrypt/config/live/ampbyexample.com/cert.pem ]; then
-  ./certbot-auto certonly --config-dir config --logs-dir logs --work-dir work --agree-tos --manual-public-ip-logging-ok -m mjs@beebo.org -n --webroot --webroot-path /letsencrypt/public -d ampbyexample.com
-fi
+# Collect/install certificates manually for now
+#if [ ! -f /letsencrypt/config/live/ampbyexample.com/cert.pem ]; then
+#  ./certbot-auto certonly --config-dir config --logs-dir logs --work-dir work --agree-tos --manual-public-ip-logging-ok -m mjs@beebo.org -n --webroot --webroot-path /letsencrypt/public -d ampbyexample.com
+#fi
 
+# TODO Only run if the certs exist
 nohup amppkg -config /letsencrypt/amppkg.toml &
 
+# Run forever; as soon as this script terminates, the container is stopped
 tail -f /dev/null
